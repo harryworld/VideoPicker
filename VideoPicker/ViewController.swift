a8733c870d34a3af512be1d02e4fe1549ed7e468
Oleg Ulitsionok <oulitsionok@aloggers.com>
2015-10-08 19:28:00 +0300

    Initial Commit

---
+++ VideoPicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoPicker
+//
+//  Created by Alex Troyanskij on 10/8/15.
+//  Copyright © 2015 STAY REAL LIMITED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
